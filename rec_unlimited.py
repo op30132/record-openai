52ade6c6342b7f69214637aac399133138ebcc29
winnie.chen <op30132@gmail.com>
2023-03-08 00:55:10 +0800

    fix: 修正偶爾斷掉問題

--- rec_unlimited.py
+++ rec_unlimited.py
@@ -12,8 +12,9 @@ from rich import print
 import keyboard
 import threading
 from datetime import datetime, timedelta
-import time as t
+import time
 import shutil
+from static_vars import static_vars
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -52,11 +53,11 @@ def rms(frame):
 def send_api_request(filename):
     if not os.path.exists(filename):
         return
+    length = 0
     with sf.SoundFile(filename, 'r') as f:
         length = len(f) / f.samplerate
     if length < 1:
         os.remove(filename)
-        print("Recording too short, try again")
         return
     text = gpt_transcript(filename)
     print(text)
@@ -86,8 +87,6 @@ def main():
         '-s', '--subtype', type=str, help='sound file subtype (e.g. "PCM_24")')
     parser.add_argument(
         "-t", "--threshold", type=float, help="RMS threshold for detecting sound (default: 0.01)", default=0.01)
-    parser.add_argument(
-        "-n", "--duration", type=float, help="Duration of sound above threshold to trigger recording stop (default: 3.0)", default=3.0)
     args = parser.parse_args(remaining)
     q = queue.Queue()
     
@@ -95,21 +94,6 @@ def main():
         device_info = sd.query_devices(args.device, 'input')
         # soundfile expects an int, sounddevice provides a float:
         args.samplerate = int(device_info['default_samplerate'])
-
-    def callback(indata, frames, time, status):
-        if status:
-            print(status, file=sys.stderr)
-            return
-        q.put(indata.copy())
-        rms = numpy.sqrt(numpy.mean(indata**2))
-        if rms > args.threshold:
-            callback.last_sound_time = datetime.now()
-        else:
-            if callback.last_sound_time is not None and (datetime.now() - callback.last_sound_time).total_seconds() > 0.95:
-                callback.last_sound_time = None
-                raise sd.CallbackAbort
-
-    callback.last_sound_time = None
     
     try:
         if not os.path.exists('record'):
@@ -123,26 +107,42 @@ def main():
                     print('#' * 80)
                     print('press Ctrl+C to stop the recording')
                     print('#' * 80)
-                    while not stop_recording:
-                        now = datetime.now()
-                        filename = "./record/" + now.strftime("%Y-%m-%d_%H-%M-%S") + ".wav"
+
+                    @static_vars(last_sound_time=None, isBreak=False)
+                    def callback(indata, frames, time, status):
+                        if status:
+                            print(status, file=sys.stderr)
+                        q.put(indata.copy())
+                        rms = numpy.sqrt(numpy.mean(indata**2))
+                        if rms > args.threshold:
+                            callback.last_sound_time = datetime.now()
+                        else:
+                            if callback.last_sound_time is not None and (datetime.now() - callback.last_sound_time).total_seconds() > 0.8:
+                                callback.last_sound_time = None
+                                callback.isBreak = True
+
+                    def open_new_file(q):
+                        filename = "./record/" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".wav"
                         with sf.SoundFile(filename, mode='x', samplerate=args.samplerate,
                             channels=args.channels) as file:
-                            def finished_callback():
-                                t = threading.Thread(target=send_api_request, args=(filename,))
-                                t.start()
-                                t.join()
-                                return
-                            with sd.InputStream(samplerate=args.samplerate, device=args.device,
-                                channels=args.channels, callback=callback, finished_callback=finished_callback) as stream:
-                                while stream.active and not stop_recording:
-                                    file.write(q.get())
+                            while True:
+                                if q.empty():
+                                    break
+                                file.write(q.get())
+                        send_api_request(filename)
+                        return
+                    with sd.InputStream(samplerate=args.samplerate, device=args.device,
+                        channels=args.channels, callback=callback) as stream:
+                        while not stop_recording:
+                            while not callback.isBreak:
+                                time.sleep(0.5)
+                            t = threading.Thread(target=open_new_file, args=(q,))
+                            t.start()
+                            q = queue.Queue()
+                            callback.isBreak = False
 
                 except KeyboardInterrupt:
                     stop_recording = True
-                    t = threading.Thread(target=send_api_request, args=(filename,))
-                    t.start()
-                    t.join()
                     print("Press 's' to start recording again")
                     print("Press 'q' to quit")
             if cmd== "q":
