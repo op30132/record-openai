fa5488ad577f8a5aefd8560905a4fb008adb6f7f
winnie.chen <op30132@gmail.com>
2023-04-12 00:53:31 +0800

    feat: add youtube summarize

--- rec_unlimited.py
+++ rec_unlimited.py
@@ -16,6 +16,9 @@ import time
 import shutil
 from static_vars import static_vars
 from dotenv import load_dotenv
+import subprocess
+import moviepy.editor as mp
+import tiktoken
 
 load_dotenv()
 
@@ -63,8 +66,115 @@ def send_api_request(filename):
     print(text)
     return
 
+def count_tokens(string: str):
+    enc = tiktoken.get_encoding("cl100k_base")
+    num_tokens = len(enc.encode(string))
+    return num_tokens
+
+
+def split_video(video_path, duration, chunk_size):
+    video = mp.VideoFileClip(video_path)
+    chunks = []
+
+    for i in range(0, duration, chunk_size):
+        start = i
+        end = min(i + chunk_size, duration)
+        chunk = video.subclip(start, end)
+        chunks.append(chunk)
+
+    return chunks
+
+def gpt_large_file_transcript(file_path, ext="mp3", chunk_size=5 * 60):
+    video = mp.VideoFileClip(file_path)
+    duration = int(video.duration)
+    if duration < chunk_size:
+        with open(file_path, "rb") as audio_file:
+            response = openai.Audio.transcribe("whisper-1", audio_file)
+            transcript = response["text"]
+        return transcript
+    video_chunks = split_video(file_path, duration, chunk_size)
+    transcript = ""
+    for i, chunk in enumerate(video_chunks):
+        temp_file = f"temp_chunk_{i}.{ext}"
+        chunk.audio.write_audiofile(temp_file)
+        with open(temp_file, "rb") as audio_file:
+            response = openai.Audio.transcribe("whisper-1", audio_file)
+            chunk_transcript = response["text"]
+        transcript += chunk_transcript
+        os.remove(temp_file)
+    return transcript
+
+def download_video(url, private=False, ext="mp3"):
+    cookies_option = "--cookies-from-browser chrome" if private else ""
+    ext_option = f"-x --audio-format {ext}" if ext == "mp3" else "--merge-output-format mp4"
+    command = f"yt-dlp {cookies_option} -o \"video.%(ext)s\" {ext_option} {url}"
+    subprocess.run(command, shell=True, check=True)
+
+def gpt_summary(text, max_chunk_tokens=1948):
+    tokens = count_tokens(text)
+    num_chunks = (tokens + max_chunk_tokens - 1) // max_chunk_tokens
+    chunk_size = len(text) // num_chunks
+    if chunk_size == 0:
+        chunks = [text]
+    else:
+        chunks = [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
+    summary = ""
+    for idx, chunk in enumerate(chunks):
+        if idx != len(chunks) - 1:
+            prompt = """
+            Your task is to summarize the text at least 500 words, and retain the keyword like book title or terms. Reply in traditional Chinese. The text is this:        
+            %s
+            """ % (summary+chunk)
+        else:
+            prompt = """
+Your output should use the following template:
+### Summary
+
+### Facts
+- Bulletpoint
+
+Your task is to start with a short summary and Summarize related sentences into the same bullet points at least 600 words. Remember retain the keyword like book title or terms. Reply in traditional chinese. The text is this:
+            %s
+            """ % (summary+chunk)
+        print(count_tokens(prompt))
+        prompt_tokens = count_tokens(prompt)
+        try :
+            response = openai.ChatCompletion.create(
+                max_tokens=(4000-prompt_tokens),
+                top_p=0.7,
+                temperature=0.7,
+                model="gpt-3.5-turbo",
+                messages=[{"role":"user","content":prompt}]
+            )
+            summary = response.choices[0].message.content
+        except:
+            response = openai.ChatCompletion.create(
+                max_tokens=(3500-prompt_tokens),
+                top_p=0.7,
+                temperature=0.7,
+                model="gpt-3.5-turbo",
+                messages=[{"role":"user","content":prompt}]
+            )
+        summary = response.choices[0].message.content
+    return summary
+
 def main():
     parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("search", help="search command")
+    parser.add_argument("-p", '--private', help="Use this flag if the video is private", action='store_true', default=False)
+    args = parser.parse_args()
+    if args.search == "search":
+        while True:
+            url = input("Enter the youtube url: ")
+            if url == "exit":
+                break
+            temp_file = f"video.mp4"
+            os.remove(temp_file) if os.path.exists(temp_file) else None
+            download_video(url, args.private, "mp4")
+            text = gpt_large_file_transcript(temp_file)
+            summary = gpt_summary(text)
+            print(summary)
+            parser.exit(0)
     parser.add_argument(
         '-l', '--list-devices', action='store_true',
         help='show list of audio devices and exit')
@@ -117,7 +227,7 @@ def main():
                         if rms > args.threshold:
                             callback.last_sound_time = datetime.now()
                         else:
-                            if callback.last_sound_time is not None and (datetime.now() - callback.last_sound_time).total_seconds() > 0.8:
+                            if callback.last_sound_time is not None and (datetime.now() - callback.last_sound_time).total_seconds() > 1:
                                 callback.last_sound_time = None
                                 callback.isBreak = True
 
